a4fdcd64671549961180cb063a1648e05df0999f
V-Coach Central <vcoach.central@gmail.com>
2025-07-21 14:18:57 +0200

    Update App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center', fontSize: '24px', color: 'black' }}>
+      <h1>Hello from V-Coach Central!</h1>
+      <p>If you can see this, the basic setup is working.</p>
+    </div>
+  );
+}
+
+export default App;

d156285eee2dadc428d429b91bd59f656a00223e
V-Coach Central <vcoach.central@gmail.com>
2025-07-21 14:39:59 +0200

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css' // Assuming you have a basic CSS file
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);

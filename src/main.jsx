fa6f9d65ac80cc4020829b0933a28dd87098c8cf
V-Coach Central <vcoach.central@gmail.com>
2025-07-28 16:36:58 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,19 @@ import App from './App.jsx'
 import './index.css'
 import { AuthProvider } from './AuthContext.jsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 // Create a client
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </QueryClientProvider>
+    <Router>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </QueryClientProvider>
+    </Router>
   </React.StrictMode>,
 )
